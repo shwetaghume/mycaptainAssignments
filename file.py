bb5b487bc350587f84af6e752ebe9c98349bca5e
shwetaghume <113381514+shwetaghume@users.noreply.github.com>
2022-10-22 00:41:04 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+
+
+n=input("enter the filename:")
+f=n.split(".")
+print("the extension of file is:" + f[-1])
+
