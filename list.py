054327961f4a4d99b2acd1e909e77b3900a12952
shwetaghume <113381514+shwetaghume@users.noreply.github.com>
2022-10-27 18:43:01 +0530

    created list.py
    all positive numbers in list

---
+++ list.py
@@ -0,0 +1,13 @@
+list1 = [12, -7, 5, 64, -14]
+list2 =[12, 14, -95, 3]
+list3=[]
+list4=[]
+for i in list1:
+    if i > 0:    
+        list3.append(i)
+for j in list2:
+    if j>0:
+        list4.append(j)
+print(list3)
+print(list4)
+
