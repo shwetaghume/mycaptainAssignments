8212d826e6d1cbf395ac7845260676ef32f072a9
shwetaghume <113381514+shwetaghume@users.noreply.github.com>
2022-10-27 18:07:15 +0530

    created fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+# Fibonacci series using recursion
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+if __name__ == "__main__":
+    n = 12
+    print(fibonacci(n))
+
